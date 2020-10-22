0ea944831afb867760593bdcc5f2dc53d050eb33
adityak-bit <adityajsr1998@gmail.com>
2020-10-22 08:54:06 +0530

    [ADITYA] Refactored code to handle Single Responsibility by separating the OpenCSV Builder

---
+++ main/java/com/QuickStart/OpenCSVBuilder.java
@@ -0,0 +1,25 @@
+package com.QuickStart;
+
+import java.io.Reader;
+import java.util.Iterator;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class OpenCSVBuilder {
+
+	public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
+		try {
+			CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			CsvToBean<E> csvToBean = csvToBeanBuilder
+					                 .withType(csvClass)
+					                 .withIgnoreLeadingWhiteSpace(true).build();
+			Iterator<E> CSVIterator = csvToBean.iterator();
+			return csvToBean.iterator();
+		} catch (RuntimeException e) {
+			throw new CensusAnalyserException(e.getMessage(),
+					CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+		}
+	}
+	
+}
