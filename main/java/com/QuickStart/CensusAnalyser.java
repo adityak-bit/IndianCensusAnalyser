9813341eabe5df344ddcb17228287bcd17518a60
adityak-bit <adityajsr1998@gmail.com>
2020-10-22 07:18:51 +0530

    [ADITYA] Refactored to close the reader

--- main/java/com/QuickStart/CensusAnalyser.java
+++ main/java/com/QuickStart/CensusAnalyser.java
@@ -3,6 +3,7 @@ package com.QuickStart;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.StreamSupport;
@@ -11,10 +12,9 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class CensusAnalyser {
-
+	
 	public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException, IOException {
-		try {
-			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))){
 			CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
 
 			CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.withType(IndiaCensusCSV.class)
